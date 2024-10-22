2910600c180b64edf4c7e51b800c54b40ec9e00a
AbdulSayyed <s.ghafoor@outlook.com>
2024-10-22 16:47:42 +0100

    cobra added to be used, main.go file changed to use cobra

--- main.go
+++ main.go
@@ -2,9 +2,27 @@ package main
 
 import (
     "fmt"
+    "os"
+    "github.com/spf13/cobra"
     "github.com/AbdulSayyed/go-lib-examp/modc"
 )
 
 func main() {
-    fmt.Println(modc.SayHelloC())
+    // Create a new Cobra root command
+    var rootCmd = &cobra.Command{
+        Use:   "greet",
+        Short: "Greets from all modules",
+        Long:  `This command greets from Module A, Module B, and Module C.`,
+        Run: func(cmd *cobra.Command, args []string) {
+            // This is where we call the function from modc
+            fmt.Println(modc.SayHelloC())
+        },
+    }
+
+    // Execute the Cobra command
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
 }
+
